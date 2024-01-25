7de18cd93025bd55dbaa44b536010d1f681c9044
Sheshadri-Chamarty <sheshadri.chamarty@gmail.com>
2024-01-25 12:39:07 +0530

    Added Examination Service. Updated auth routes endpoints

--- backend/authentication-service/server.js
+++ backend/authentication-service/server.js
@@ -1,42 +1,47 @@
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
-const cookieParser = require("cookie-parser")
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const cookieParser = require("cookie-parser");
 
-require("../shared/config/config")
+// require("../shared/config/config");
 
-const ConnectToDatabase = require("../shared/admin/admin")
+// const ConnectToDatabase = require("../shared/admin/admin");
 
-const app = express()
+// const app = express();
 
-const corsOptions = {
-  origin: `${process.env.FRONDEND_LINK}`,
-  credentials: true, 
-  optionSuccessStatus: 200,
-  Headers: true,
-  exposedHeaders: 'Set-Cookie',
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Content-Type',
-    'Authorization'
-  ]
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: `${process.env.FRONDEND_LINK}`,
+//   credentials: true,
+//   optionSuccessStatus: 200,
+//   Headers: true,
+//   exposedHeaders: "Set-Cookie",
+//   methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+//   allowedHeaders: [
+//     "Access-Control-Allow-Origin",
+//     "Content-Type",
+//     "Authorization",
+//   ],
+// };
+// app.use(cors(corsOptions));
 
-app.use(morgan("dev"))
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/student/", require("./routes/students.routes"))
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(cookieParser());
+
+const app = require("../init/init");
+
+const ConnectToDatabase = require("../shared/admin/admin");
+
+app.use("/api/student/", require("./routes/students.routes"));
 
 app.get("/", (_, res) => {
-  res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
-ConnectToDatabase()
+ConnectToDatabase();
 
-const PORT = process.env.AUTHENTICATION_PORT || 5001
+const PORT = process.env.AUTHENTICATION_PORT || 9001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ backend/examination-service/routes/exams.routes.js
@@ -0,0 +1,106 @@
+const router = require("express").Router();
+
+const Exam = require("../../shared/models/exam.model");
+const authenticate = require("../../shared/middleware/authenticate");
+
+router.route("/all").get(async (req, res) => {
+  try {
+    const exams = await Exam.find();
+    if (!exams.length) {
+      return res.status(400).json({
+        msg: "No Exam found.",
+      });
+    }
+    res.status(200).json({ exams });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+
+router.route("/create").post(async (req, res) => {
+  try {
+    const { title, description, topic, duration, questions } = req.body;
+    const newExam = new Exam({
+      title,
+      duration,
+      description,
+      topic,
+      questions,
+    });
+    const savedExam = await newExam.save();
+    res.status(201).json({ msg: "Exam creation successful!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+
+router.route("/delete/all").delete(async (req, res) => {
+  try {
+    const deletedExams = await Exam.deleteMany({});
+    if (deletedExams.deletedCount === 0) {
+      return res.status(400).json({
+        msg: "No exams found to delete.",
+      });
+    }
+    res.status(200).json({ msg: "All exams deleted successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+
+router.route("/delete/:examId").delete(async (req, res) => {
+  try {
+    const { examId } = req.params;
+    const exam = await Exam.findById({ _id: examId });
+    if (!exam) {
+      return res.status(400).json({
+        msg: "No Exam found.",
+      });
+    }
+    const deletedExam = await Exam.findByIdAndDelete({ _id: examId });
+    res.status(200).json({ msg: "Exam deletion successful!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+
+router.route("/topic/:topic").get(async (req, res) => {
+  try {
+    const { topic } = req.params;
+    const exams = await Exam.find({ topic: topic });
+    if (!exams) {
+      return res.status(400).json({
+        msg: "No Exam found.",
+      });
+    }
+    res.status(200).json({ exams });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+router.route("/submit").post(async (req, res) => {
+  try {
+    const { studentId, examId, questions } = req.body;
+    let answers = await Exam.findById({ _id: examId });
+    if (!answers) {
+      return res.status(401).send("No Exam found");
+    }
+    answers = answers.questions;
+    let marks = 0;
+    for (i = 0; i < questions.length; i++) {
+      if (questions[i].answer === answers[i].answer) {
+        marks++;
+      }
+    }
+    return res.status(200).json({ marks });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
+});
+module.exports = router;

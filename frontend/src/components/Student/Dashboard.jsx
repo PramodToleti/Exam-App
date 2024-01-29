1347dcf482af8c7915fcb70b57128cd79cd10073
PramodToleti <pramodraina047@gmail.com>
2024-01-29 18:08:18 +0530

    Add links to exams and display grouped results

--- frontend/src/components/Student/Dashboard.jsx
+++ frontend/src/components/Student/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Navbar from "./Navbar"
 
 const Dashboard = () => {
@@ -11,7 +12,17 @@ const Dashboard = () => {
           Welcome back, {user?.username ?? "User"} 👋
         </h1>
 
-        
+        <div className="text-gray-800 dark:text-white">
+          <h2 className="text-xl font-medium mb-4 mt-10">{`Today's Exams`}</h2>
+          <div className="flex flex-col items-center justify-center min-h-96">
+            <p className="text-lg font-medium mb-4">No exams today ☹️</p>
+            <Link to="/exams">
+              <button className="flex justify-center items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700 mt-4">
+                View all exams
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   )
--- frontend/src/components/Student/Results.jsx
+++ frontend/src/components/Student/Results.jsx
@@ -23,6 +23,22 @@ const Results = () => {
     getResult()
   }, [studentId])
 
+  const groupedResults = results
+    .slice()
+    .reverse()
+    .reduce((acc, result) => {
+      const submissionDate = new Date(
+        result.submissionTime
+      ).toLocaleDateString()
+      if (!acc[submissionDate]) {
+        acc[submissionDate] = []
+      }
+      acc[submissionDate].push(result)
+      return acc
+    }, {})
+
+  const today = new Date().toLocaleDateString()
+
   return (
     <section className="bg-slate-100 dark:bg-gray-900 py-4 md:py-10 min-h-screen">
       <div className="container mx-auto px-4">
@@ -39,57 +55,80 @@ const Results = () => {
               <h1 className="text-3xl font-medium text-gray-800 dark:text-gray-100 mb-8">
                 Results
               </h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {results
-                  .slice()
-                  .reverse()
-                  .map((result, index) => (
-                    <div
-                      className="bg-white text-dark dark:text-white dark:bg-slate-800 rounded-lg shadow-md p-4 mb-4 max-w-lg w-full flex flex-col items-center"
-                      key={index}
-                    >
-                      <h1 className="text-xl font-medium mb-4">
-                        {result.title}
-                      </h1>
-                      <p className="mb-4">{result.topic}</p>
-                      <div className="flex flex-col md:flex-row justify-between">
-                        <div className="flex flex-col">
-                          <h2 className="text-4xl font-medium self-center mb-6 mt-4 h-12 text-center  rounded-full">
-                            {result.noOfCorrectAnswers} / {result.totalMarks}
-                          </h2>
-                          <div className="flex flex-col md:flex-row justify-between gap-6 mb-4">
-                            <div className="flex flex-col gap-3 items-center">
-                              <p className=" text-lg">Correct answers</p>
-                              <p className="font-medium text-lg md:text-xl">
-                                {result.noOfCorrectAnswers}
-                              </p>
+
+              {Object.entries(groupedResults).map(([date, resultsArray]) => (
+                <div key={date}>
+                  {date === today && (
+                    <h2 className="text-2xl dark:text-white mb-4">Today</h2>
+                  )}
+                  {date !== today &&
+                    date ===
+                      new Date(
+                        new Date().setDate(new Date().getDate() - 1)
+                      ).toLocaleDateString() && (
+                      <h2 className="text-2xl dark:text-white mb-4">
+                        Yesterday
+                      </h2>
+                    )}
+                  {date !== today &&
+                    date !==
+                      new Date(
+                        new Date().setDate(new Date().getDate() - 1)
+                      ).toLocaleDateString() && (
+                      <h2 className="text-2xl dark:text-white mb-4">
+                        Previous
+                      </h2>
+                    )}
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {resultsArray.map((result, index) => (
+                      <div
+                        className="bg-white text-dark dark:text-white dark:bg-slate-800 rounded-lg shadow-md p-4 mb-4 max-w-lg w-full flex flex-col items-center"
+                        key={index}
+                      >
+                        <h1 className="text-xl font-medium mb-4">
+                          {result.title}
+                        </h1>
+                        <p className="mb-4">{result.topic}</p>
+                        <div className="flex flex-col md:flex-row justify-between">
+                          <div className="flex flex-col">
+                            <h2 className="text-4xl font-medium self-center mb-6 mt-4 h-12 text-center  rounded-full">
+                              {result.noOfCorrectAnswers} / {result.totalMarks}
+                            </h2>
+                            <div className="flex flex-col md:flex-row justify-between gap-6 mb-4">
+                              <div className="flex flex-col gap-3 items-center">
+                                <p className=" text-lg">Correct answers</p>
+                                <p className="font-medium text-lg md:text-xl">
+                                  {result.noOfCorrectAnswers}
+                                </p>
+                              </div>
+                              <div className="flex flex-col gap-3 items-center">
+                                <p className="text-lg">Wrong answers</p>
+                                <p className=" font-medium text-lg md:text-xl">
+                                  {result.noOfWrongAnswers}
+                                </p>
+                              </div>
                             </div>
-                            <div className="flex flex-col gap-3 items-center">
-                              <p className="text-lg">Wrong answers</p>
-                              <p className=" font-medium text-lg md:text-xl">
-                                {result.noOfWrongAnswers}
-                              </p>
-                            </div>
+                            <p className="text-center text-sm mt-4">
+                              Submitted at:{" "}
+                              {new Date(result.submissionTime).toLocaleString(
+                                undefined,
+                                {
+                                  year: "numeric",
+                                  month: "numeric",
+                                  day: "numeric",
+                                  hour: "2-digit",
+                                  minute: "2-digit",
+                                  second: "2-digit",
+                                }
+                              )}
+                            </p>
                           </div>
-                          <p className="text-center text-sm mt-4">
-                            Submitted at:{" "}
-                            {new Date(result.submissionTime).toLocaleString(
-                              undefined,
-                              {
-                                year: "numeric",
-                                month: "numeric",
-                                day: "numeric",
-                                hour: "2-digit",
-                                minute: "2-digit",
-                                second: "2-digit",
-                              }
-                            )}
-                          </p>
                         </div>
                       </div>
-                    </div>
-                  ))}
-              </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
           )
         )}
